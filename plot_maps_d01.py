5ac15ca1087fd45c8a020f98aa1d11e46e32e586
Bill Capehart <William.Capehart@sdsmt.edu>
2021-10-12 22:22:09 -0600

    fix snow on d01 priduct

--- plot_maps_d01.py
+++ plot_maps_d01.py
@@ -891,7 +891,7 @@ for domain in range(1,1+1):
 
         filled_cm     = ax1.contourf(lon2d, 
                                      lat2d, 
-                                     hrly_snowfall_map.isel(Time=t),
+                                     hrly_snowfall_maps.isel(Time=t),
                                      transform = ccrs.PlateCarree(),
                                      norm      = rain_norm,
                                      cmap      = precip_colormap,
