f00e2bd42c53899b9742b33a552880b1335f9005
Bill Capehart <William.Capehart@sdsmt.edu>
2021-10-16 22:05:19 -0600

    diagnosting font

--- plot_tslist_meteograms.py
+++ plot_tslist_meteograms.py
@@ -48,8 +48,12 @@ sns.set_theme(style="ticks")
 
 if (platform.system() != "Darwin"):
     path = '/usr/share/fonts/truetype/open-sans/OpenSans-Regular.ttf'
+    print("          Enabling OpenSans " + path)
     prop = fm.FontProperties(fname=path)
+    print( prop )
     mpl.rcParams['font.family'] = prop.get_name()
+    print("mpl.rcParams['font.family'] " + mpl.rcParams['font.family'])
+
 
 #
 ####################################################
