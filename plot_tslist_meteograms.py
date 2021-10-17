4c98de4da2d34ffe3c6d9af39bba2df296264e25
Bill Capehart <William.Capehart@sdsmt.edu>
2021-10-16 22:10:17 -0600

    diagnosting opensans font

--- plot_tslist_meteograms.py
+++ plot_tslist_meteograms.py
@@ -51,7 +51,7 @@ if (platform.system() != "Darwin"):
     print("          Enabling OpenSans " + path)
     prop = fm.FontProperties(fname=path)
     print( prop.get_name() )
-    mpl.rcParams['font.family'] = prop.get_name()
+    mpl.rcParams['font.family'] = prop.get_name()[0]
     print("mpl.rcParams['font.family'] " + mpl.rcParams['font.family'])
 
 
