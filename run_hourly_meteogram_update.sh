7b7bbbd637497b5b69b48d4a3f5e93559e9991fe
Bill Capehart <William.Capehart@sdsmt.edu>
2021-10-24 00:44:44 -0600

    scropts for chrons

--- run_hourly_meteogram_update.sh
+++ run_hourly_meteogram_update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-(
 echo "Date: `date`"
-DATESTRING=`date +"%Y-%m-%d"`
+DATESTRING=`date +"%Y-%m-%d_%H%M"`
+echo $DATESTRING
 source /home/wjc/.bashrc
 cd /home/wjc/GitHub/SD_Mines_WRF_REALTIME
 which python
 python /home/wjc/GitHub/SD_Mines_WRF_REALTIME/plot_tslist_meteograms_update.py 2>&1 /home/wjc/GitHub/SD_Mines_WRF_REALTIME/wrf_metogramupdate_${DATESTRING}.log
 echo "Completed: `date`"
 echo
-) >>${LOGFILE:=wrf_metogramupdate_log} 2>&1 &
+
 
 
--- run_realtime_wrf_system.sh
+++ run_realtime_wrf_system.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-(
 echo "Date: `date`"
 source /home/wjc/.bashrc
 DATESTRING=`date +"%Y-%m-%d"`
@@ -7,6 +6,5 @@ cd /home/wjc/GitHub/SD_Mines_WRF_REALTIME
 python /home/wjc/GitHub/SD_Mines_WRF_REALTIME/WRF_Backbone_Script.py 2>&1 /home/wjc/GitHub/SD_Mines_WRF_REALTIME/wrf_log_${DATESTRING}.log
 echo "Completed: `date`"
 echo
-) >>${LOGFILE:=log_wrf_realtime} 2>&1 &
 
 
