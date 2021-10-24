368ef90fc9b6c9eb4a099f7c12d78793a40f422d
Bill Capehart <William.Capehart@sdsmt.edu>
2021-10-24 00:35:59 -0600

    iut

--- run_hourly_meteogram_update.sh
+++ run_hourly_meteogram_update.sh
@@ -4,6 +4,7 @@ echo "Date: `date`"
 DATESTRING=`date +"%Y-%m-%d"`
 source /home/wjc/.bashrc
 cd /home/wjc/GitHub/SD_Mines_WRF_REALTIME
+which python
 python /home/wjc/GitHub/SD_Mines_WRF_REALTIME/plot_tslist_meteograms_update.py 2>&1 /home/wjc/GitHub/SD_Mines_WRF_REALTIME/wrf_metogramupdate_${DATESTRING}.log
 echo "Completed: `date`"
 echo
