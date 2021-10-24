4d787959dff4f06ae014c59795e14739408cdbe1
Bill Capehart <William.Capehart@sdsmt.edu>
2021-10-24 00:52:38 -0600

    patch

--- run_hourly_meteogram_update.sh
+++ run_hourly_meteogram_update.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 echo "Date: `date`"
 DATESTRING=`date +"%Y-%m-%d_%H%M"`
+
+
+
+__conda_setup="$('/home/wjc/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/wjc/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/wjc/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/wjc/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+
+which python
+
+
+
 echo $DATESTRING
 source /home/wjc/.bashrc
 cd /home/wjc/GitHub/SD_Mines_WRF_REALTIME
--- run_realtime_wrf_system.sh
+++ run_realtime_wrf_system.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 echo "Date: `date`"
 source /home/wjc/.bashrc
-DATESTRING=`date +"%Y-%m-%d"`
+
+__conda_setup="$('/home/wjc/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/wjc/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/wjc/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/wjc/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+
+which python
+
+
+DATESTRING=`date +"%Y-%m-%d%H"`
 cd /home/wjc/GitHub/SD_Mines_WRF_REALTIME
 python /home/wjc/GitHub/SD_Mines_WRF_REALTIME/WRF_Backbone_Script.py 2>&1 /home/wjc/GitHub/SD_Mines_WRF_REALTIME/wrf_log_${DATESTRING}.log
 echo "Completed: `date`"
