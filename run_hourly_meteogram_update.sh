6eb28314f633e877bbd4265143a4aacbf00ef090
Bill Capehart <William.Capehart@sdsmt.edu>
2021-10-24 10:30:02 -0600

    conda script debugging

--- run_hourly_meteogram_update.sh
+++ run_hourly_meteogram_update.sh
@@ -2,24 +2,10 @@
 echo on
 echo "Date: `date`"
 
-__conda_setup="$('/home/wjc/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/home/wjc/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/wjc/miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/home/wjc/miniconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-
-conda activate base
 
 echo $DATESTRING
 cd /home/wjc/GitHub/SD_Mines_WRF_REALTIME
-which python
-python /home/wjc/GitHub/SD_Mines_WRF_REALTIME/plot_tslist_meteograms_update.py 2>&1 /home/wjc/GitHub/SD_Mines_WRF_REALTIME/wrf_metogramupdate_${DATESTRING}.log
+/home/wjc/miniconda3/bin/python /home/wjc/GitHub/SD_Mines_WRF_REALTIME/plot_tslist_meteograms_update.py 2>&1 /home/wjc/GitHub/SD_Mines_WRF_REALTIME/wrf_metogramupdate_${DATESTRING}.log
 echo "Completed: `date`"
 echo
 echo off
--- run_realtime_wrf_system.sh
+++ run_realtime_wrf_system.sh
@@ -2,23 +2,10 @@
 echo on
 echo "Date: `date`"
 
-__conda_setup="$('/home/wjc/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/home/wjc/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/wjc/miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/home/wjc/miniconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-
-conda activate base
 
 DATESTRING=`date +"%Y-%m-%d%H"`
 cd /home/wjc/GitHub/SD_Mines_WRF_REALTIME
-python /home/wjc/GitHub/SD_Mines_WRF_REALTIME/WRF_Backbone_Script.py 2>&1 /home/wjc/GitHub/SD_Mines_WRF_REALTIME/wrf_log_${DATESTRING}.log
+/home/wjc/miniconda3/bin/python /home/wjc/GitHub/SD_Mines_WRF_REALTIME/WRF_Backbone_Script.py 2>&1 /home/wjc/GitHub/SD_Mines_WRF_REALTIME/wrf_log_${DATESTRING}.log
 echo "Completed: `date`"
 echo
 echo off
