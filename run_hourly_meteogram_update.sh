ef0e2c22e6af55934e98436209979d16851f0996
Bill Capehart <William.Capehart@sdsmt.edu>
2021-10-25 09:29:40 -0600

    changing wrf run script for MPIEXEX Capture

--- run_hourly_meteogram_update.sh
+++ run_hourly_meteogram_update.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 echo on
+echo
+echo "Starting Plotting Script"
+echo
 echo "Date: `date`"
-
-
+echo
+echo "Getting Date Stamps"
+echo
 echo $DATESTRING
+echo
+echo "Moving to Working Directory"
+echo
 cd /home/wjc/GitHub/SD_Mines_WRF_REALTIME
+echo
+echo "Starting Python Script"
+echo
 /home/wjc/miniconda3/bin/python /home/wjc/GitHub/SD_Mines_WRF_REALTIME/plot_tslist_meteograms_update.py 2>&1 /home/wjc/GitHub/SD_Mines_WRF_REALTIME/wrf_metogramupdate_${DATESTRING}.log
+echo
+echo "Ending Script"
+echo
 echo "Completed: `date`"
 echo
 echo off
-
-
--- run_realtime_wrf_system.sh
+++ run_realtime_wrf_system.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 echo on
+echo
+echo "Starting WRF Realtime Execution"
 echo "Date: `date`"
-
-
+echo
+echo
 DATESTRING=`date +"%Y-%m-%d%H"`
+echo
+echo Running BashRC
+echo
+. ~/.bashrc
+echo
+echo "Checking for MPIRUN and MPIEXEX"
+echo
+which mpirun
+which mpiexec
+echo
+echo "Entering Working Directory"
+echo
 cd /home/wjc/GitHub/SD_Mines_WRF_REALTIME
+echo
+echo  "Firing Things Up!"
+echo
 /home/wjc/miniconda3/bin/python /home/wjc/GitHub/SD_Mines_WRF_REALTIME/WRF_Backbone_Script.py 2>&1 /home/wjc/GitHub/SD_Mines_WRF_REALTIME/wrf_log_${DATESTRING}.log
+echo
+echo "Ending Script"
+echo
 echo "Completed: `date`"
 echo
 echo off
-
