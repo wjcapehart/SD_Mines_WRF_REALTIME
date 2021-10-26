ca07ef67a3ae31952035e75fa4f6ec21adb880c9
Bill Capehart <William.Capehart@sdsmt.edu>
2021-10-25 18:57:31 -0600

    updating mpich scripting

--- run_realtime_wrf_system.sh
+++ run_realtime_wrf_system.sh
@@ -10,10 +10,6 @@ echo
 echo Running BashRC
 echo
 echo
-echo "Checking for MPIRUN and MPIEXEX"
-echo
-which mpirun > /home/wjc/wheres_my_mpirun.txt 2>&1
-which mpiexec > /home/wjc/wheres_my_mpiexec.txt 2>&1 
 echo
 echo "Entering Working Directory"
 echo
@@ -21,7 +17,7 @@ cd /home/wjc/GitHub/SD_Mines_WRF_REALTIME
 echo
 echo  "Firing Things Up!"
 echo
-/home/wjc/miniconda3/bin/python /home/wjc/GitHub/SD_Mines_WRF_REALTIME/WRF_Backbone_Script.py > /home/wjc/GitHub/SD_Mines_WRF_REALTIME/wrf_log_${DATESTRING}.log 2>&1
+. source ~/.bashrc ; /home/wjc/miniconda3/bin/python /home/wjc/GitHub/SD_Mines_WRF_REALTIME/WRF_Backbone_Script.py > /home/wjc/GitHub/SD_Mines_WRF_REALTIME/wrf_log_${DATESTRING}.log 2>&1
 echo
 echo "Ending Script"
 echo
