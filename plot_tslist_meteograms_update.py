507538763a39ae4b63084790f79b67408f7789ba
Bill Capehart <William.Capehart@sdsmt.edu>
2021-10-31 21:40:56 -0600

    correction to meteogram update (including haversine function)

--- plot_tslist_meteograms_update.py
+++ plot_tslist_meteograms_update.py
@@ -58,6 +58,22 @@ import airportsdata as airpt
 
 ureg = pint.UnitRegistry()
 Q_ = ureg.Quantity
+
+
+def haversine(row):
+    lon1 = station_lon
+    lat1 = station_lat
+    lon2 = row['longitude']
+    lat2 = row['latitude']
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
+    c = 2 * np.arcsin(np.sqrt(a)) 
+    km = 6367 * c
+    return km
+    
+    
 #
 ####################################################
 ####################################################
