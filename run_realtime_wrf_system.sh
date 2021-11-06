885d991f6a5137026dd772c1321b1a64f91b6415
Bill Capehart <William.Capehart@sdsmt.edu>
2021-11-06 17:44:49 -0600

    adjust scripts

--- run_realtime_wrf_system.sh
+++ run_realtime_wrf_system.sh
@@ -14,7 +14,7 @@ cd /home/wjc/GitHub/SD_Mines_WRF_REALTIME
 echo
 echo  "Firing Things Up!"
 echo
-. source ~/.bashrc ; /home/wjc/miniconda3/bin/python /home/wjc/GitHub/SD_Mines_WRF_REALTIME/WRF_Backbone_Script.py > /home/wjc/GitHub/SD_Mines_WRF_REALTIME/wrf_log_${DATESTRING}.log 2>&1
+. source /home/wjc/.bashrc ; /home/wjc/miniconda3/bin/python /home/wjc/GitHub/SD_Mines_WRF_REALTIME/WRF_Backbone_Script.py > /home/wjc/GitHub/SD_Mines_WRF_REALTIME/wrf_log_${DATESTRING}.log 2>&1
 echo
 echo "Ending Script"
 echo
--- wrf_post_processing.sh
+++ wrf_post_processing.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-source ~/.bashrc
+source /home/wjc/.bashrc
 cd /Users/wjc/GitHub/SD_Mines_WRF_REALTIME/
-( python /Users/wjc/GitHub/SD_Mines_WRF_REALTIME/./tslist_to_netcdf.py  && python /Users/wjc/GitHub/SD_Mines_WRF_REALTIME/./plot_tslist_meteograms.py ) & 
-( python /Users/wjc/GitHub/SD_Mines_WRF_REALTIME/./plot_maps.py  ) & 
-( python /Users/wjc/GitHub/SD_Mines_WRF_REALTIME/./plot_skewt.py  ) & 
-( /Users/wjc/GitHub/SD_Mines_WRF_REALTIME/namelist_files_and_local_scripts/run_unipost_frames_SDMines >& UUP.LOG ) & 
+( python /Users/wjc/GitHub/SD_Mines_WRF_REALTIME/./tslist_to_netcdf.py  && python /Users/wjc/GitHub/SD_Mines_WRF_REALTIME/./plot_tslist_meteograms.py ) &
+( python /Users/wjc/GitHub/SD_Mines_WRF_REALTIME/./plot_maps.py  ) &
+( python /Users/wjc/GitHub/SD_Mines_WRF_REALTIME/./plot_skewt.py  ) &
+( /Users/wjc/GitHub/SD_Mines_WRF_REALTIME/namelist_files_and_local_scripts/run_unipost_frames_SDMines >& UUP.LOG ) &
 wait
 echo We're Outahere Like Vladimir
