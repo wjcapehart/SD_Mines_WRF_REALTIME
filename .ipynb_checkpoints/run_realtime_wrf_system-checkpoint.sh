4be65bf8b408717f57cdd977c182039f364af83a
Bill Capehart <William.Capehart@sdsmt.edu>
2022-05-31 22:09:22 -0600

    fixing main wrf script for end of wrf run time series.

--- .ipynb_checkpoints/run_realtime_wrf_system-checkpoint.sh
+++ .ipynb_checkpoints/run_realtime_wrf_system-checkpoint.sh
@@ -15,7 +15,7 @@ echo
 echo  "Firing Things Up!"
 echo
 .  /home/wjc/.bashrc ; /home/wjc/miniconda3/bin/python /home/wjc/GitHub/SD_Mines_WRF_REALTIME/WRF_Backbone_Script.py > /home/wjc/GitHub/SD_Mines_WRF_REALTIME/wrf_log_${DATESTRING}.log 2>&1
-echo
+.  /home/wjc/.bashrc ; /home/wjc/miniconda3/bin/python /home/wjc/GitHub/SD_Mines_WRF_REALTIME/plot_tslist_meteograms_update.py > /home/wjc/GitHub/SD_Mines_WRF_REALTIME/wrf_metogramupdate_${DATESTRING}.log 2>&1
 echo "Ending Script"
 echo
 echo "Completed: `date`"
