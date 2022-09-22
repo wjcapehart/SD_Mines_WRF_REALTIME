c6b01249e02bd8f9b3ba919033e85b516aeec7a5
Bill Capehart <William.Capehart@sdsmt.edu>
2022-09-22 13:59:13 -0600

    Update WRF_Backbone_Script.py

--- WRF_Backbone_Script.py
+++ WRF_Backbone_Script.py
@@ -719,7 +719,9 @@ else:
         print("export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH", file = f)
         print("export LD_LIBRARY_PATH=/usr/local/lib/::${LD_LIBRARY_PATH}", file = f)
         print("cd " + WRF_EXE, file =  f) 
-        print("mpirun -print-all-exitcodes -print-rank-map -f ~wjc/nodes.wrf.3 -np 36 -ppn 18 ./REAL.EXE 2>&1 real.log", file =  f) 
+        print("export OMP_NUM_THREADS=18")
+        print("mpirun -print-all-exitcodes -print-rank-map -f ~wjc/nodes.wrf.3 -np 2  -ppn 1  " + WRF_EXE + "./REAL_INTEL_DMSM_AVX.EXE 2>&1 real.log", file =  f) 
+        print("mpirun -print-all-exitcodes -print-rank-map -f ~wjc/nodes.wrf.3 -np 36 -ppn 18 " + WRF_EXE + "./REAL_INTEL_DM_AVX.EXE   2>&1 real.log", file =  f) 
         print("echo WRF:REAL::: We^re Outahere Like Vladimir", file =  f) 
 
     os.system("chmod a+x " + WRF_EXE + "./processing_wrf_real.sh")
@@ -778,7 +780,9 @@ else:
         print("export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH", file = f)
         print("export LD_LIBRARY_PATH=/usr/local/lib/::${LD_LIBRARY_PATH}", file = f)
         print("cd " + WRF_EXE, file =  f) 
-        print("mpirun -print-all-exitcodes -print-rank-map -f ~wjc/nodes.wrf.3 -np 36 -ppn 18  ./WRF.EXE 2>&1 wrf.log", file =  f) 
+        print("export OMP_NUM_THREADS=18")
+        print("mpirun -print-all-exitcodes -print-rank-map -f ~wjc/nodes.wrf.3 -np 2  -ppn 1  " + WRF_EXE + "./WRF_INTEL_DMSM_AVX.EXE 2>&1  wrf.log", file =  f) 
+        print("mpirun -print-all-exitcodes -print-rank-map -f ~wjc/nodes.wrf.3 -np 36 -ppn 18 " + WRF_EXE + "./WRF_INTEL_DM_AVX.EXE   2>&1  wrf.log", file =  f) 
         print("echo WRF:WRF::: We^re Outahere Like Vladimir", file =  f) 
 
     os.system("chmod a+x " + WRF_EXE + "./processing_wrf_wrf.sh")
