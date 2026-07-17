d1369b7edf0db2e3c7b82f9572174c98133ad414
Bill Capehart <William.Capehart@sdsmt.edu>
2026-07-17 16:40:31 -0600

    Create run_realtime_wrf_system_on_raid.sh

---
+++ run_realtime_wrf_system_on_raid.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo on
+echo
+echo "Starting WRF Realtime Execution"
+echo "Date: `date`"
+echo
+echo
+DATESTRING=`date +"%Y-%m-%d_%H%M"`
+echo
+echo
+echo "Entering Working Directory"
+echo
+cd /projects/SD_Mines_WRF_REALTIME
+echo
+echo  "Firing Things Up!"
+echo
+.  /home/wjc/.bash_profile ; /home/wjc/miniconda3/bin/python /projects/SD_Mines_WRF_REALTIME/WRF_Backbone_Script.py > /home/wjc/GitHub/SD_Mines_WRF_REALTIME/wrf_log_${DATESTRING}.log 2>&1
+.  /home/wjc/.bash_profile ; /home/wjc/miniconda3/bin/python /home/wjc/GitHub/SD_Mines_WRF_REALTIME/plot_tslist_meteograms_update.py > /home/wjc/GitHub/SD_Mines_WRF_REALTIME/wrf_metogramupdate_${DATESTRING}.log 2>&1
+echo "Ending Script"
+echo
+echo "Completed: `date`"
+echo
+echo off
